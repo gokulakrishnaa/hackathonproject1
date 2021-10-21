788d4150e4ad4375dd434e7a15c767bb4da971ca
gokulakrishnaa <91211698+gokulakrishnaa@users.noreply.github.com>
2021-10-21 13:41:33 +0530

    Gmail clone

---
+++ script.js
@@ -0,0 +1,32 @@
+//Enabled GMail Api in google developer console and following the steps as per docs
+
+var GoogleAuth; // Google Auth object.
+function initClient() {
+  gapi.client
+    .init({
+      apiKey: "AIzaSyBZaETX53auXyRPe5Ob5HgbCDRPuyoZUB8",
+      clientId:
+        "79489010481-gn0gnn3vtbfd9cp1tq73ruejgnbpiodk.apps.googleusercontent.com",
+      scope: "https://www.googleapis.com/auth/drive.metadata.readonly",
+      discoveryDocs: [
+        "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
+      ],
+    })
+    .then(function () {
+      GoogleAuth = gapi.auth2.getAuthInstance();
+
+      // Listen for sign-in state changes.
+      GoogleAuth.isSignedIn.listen(updateSigninStatus);
+    });
+}
+
+// Compose Email
+
+async function sendMail() {
+  const compose = document.querySelector(".btn");
+  const data = await fetch(
+    "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
+    { method: "GET" }
+  );
+  const user = await data.json();
+}
